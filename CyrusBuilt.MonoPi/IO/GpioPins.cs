02ba3f1b8e952339310432ff08cf07aa85dccc3a
cyrusbuilt <cyrusbuilt@gmail.com>
2013-05-07 23:16:28 -0400

    Updated pin documentation.
    Updated XML documentation for pin definitions.

--- CyrusBuilt.MonoPi/IO/GpioPins.cs
+++ CyrusBuilt.MonoPi/IO/GpioPins.cs
@@ -1,5 +1,5 @@
 //
-//  GpioPinsRev1.cs
+//  GpioPins.cs
 //
 //  Author:
 //       Chris Brunner <cyrusbuilt at gmail dot com>
@@ -237,88 +237,196 @@ namespace CyrusBuilt.MonoPi.IO
 		LED = 16,
 
 		#region Board Revision 2 pins
-		V2_GPIO00 = 0,
-
-		V2_GPIO01 = 1,
-
+		/// <summary>
+		/// Rev2 GPIO 02 (P1-03).
+		/// </summary>
 		V2_GPIO02 = 2,
 
+		/// <summary>
+		/// Rev2 GPIO 03 (P1-05).
+		/// </summary>
 		V2_GPIO03 = 3,
 
+		/// <summary>
+		/// Rev2 GPIO 04 (P1-07).
+		/// </summary>
 		V2_GPIO04 = 4,
 
+		/// <summary>
+		/// Rev2 GPIO 07 (P1-26).
+		/// </summary>
 		V2_GPIO07 = 7,
 
+		/// <summary>
+		/// Rev2 GPIO 08 (P1-24).
+		/// </summary>
 		V2_GPIO08 = 8,
 
+		/// <summary>
+		/// Rev2 GPIO 09 (P1-21).
+		/// </summary>
 		V2_GPIO09 = 9,
 
+		/// <summary>
+		/// Rev2 GPIO 10 (P1-19).
+		/// </summary>
 		V2_GPIO10 = 10,
 
+		/// <summary>
+		/// Rev2 GPIO 11 (P1-23).
+		/// </summary>
 		V2_GPIO11 = 11,
 
+		/// <summary>
+		/// Rev2 GPIO 14 (P1-08).
+		/// </summary>
 		V2_GPIO14 = 14,
 
+		/// <summary>
+		/// Rev2 GPIO 15 (P1-10).
+		/// </summary>
 		V2_GPIO15 = 15,
 
+		/// <summary>
+		/// Rev2 GPIO 17 (P1-11).
+		/// </summary>
 		V2_GPIO17 = 17,
 
+		/// <summary>
+		/// Rev2 GPIO 18 (P1-12).
+		/// </summary>
 		V2_GPIO18 = 18,
 
-		V2_GPIO21 = 21,
-
+		/// <summary>
+		/// Rev2 GPIO 22 (P1-15).
+		/// </summary>
 		V2_GPIO22 = 22,
 
+		/// <summary>
+		/// Rev2 GPIO 23 (P1-16).
+		/// </summary>
 		V2_GPIO23 = 23,
 
+		/// <summary>
+		/// Rev2 GPIO 24 (P1-18).
+		/// </summary>
 		V2_GPIO24 = 24,
 
+		/// <summary>
+		/// Rev2 GPIO 25 (P1-22).
+		/// </summary>
 		V2_GPIO25 = 25,
 
+		/// <summary>
+		/// Rev2 GPIO 27 (P1-13).
+		/// </summary>
 		V2_GPIO27 = 27,
 
+		/// <summary>
+		/// Rev2 Pin 3 (GPIO 02).
+		/// </summary>
 		V2_Pin03 = 2,
 
+		/// <summary>
+		/// Rev2 Pin 5 (GPIO 03).
+		/// </summary>
 		V2_Pin05 = 3,
 
+		/// <summary>
+		/// Rev2 Pin 7 (GPIO 04).
+		/// </summary>
 		V2_Pin07 = 4,
 
+		/// <summary>
+		/// Rev2 Pin 8 (GPIO 14).
+		/// </summary>
 		V2_Pin08 = 14,
 
+		/// <summary>
+		/// Rev2 Pin 10 (GPIO 15).
+		/// </summary>
 		V2_Pin10 = 15,
 
+		/// <summary>
+		/// Rev2 Pin 11 (GPIO 17).
+		/// </summary>
 		V2_Pin11 = 17,
 
+		/// <summary>
+		/// Rev2 Pin 12 (GPIO 18).
+		/// </summary>
 		V2_Pin12 = 18,
 
+		/// <summary>
+		/// Rev2 Pin 13 (GPIO 27).
+		/// </summary>
 		V2_Pin13 = 27,
 
+		/// <summary>
+		/// Rev2 Pin 15 (GPIO 22).
+		/// </summary>
 		V2_Pin15 = 22,
 
+		/// <summary>
+		/// Rev2 Pin 16 (GPIO 23).
+		/// </summary>
 		V2_Pin16 = 23,
 
+		/// <summary>
+		/// Rev2 Pin 18 (GPIO 24).
+		/// </summary>
 		V2_Pin18 = 24,
 
+		/// <summary>
+		/// Rev2 Pin 19 (GPIO 10).
+		/// </summary>
 		V2_Pin19 = 10,
 
+		/// <summary>
+		/// Rev2 Pin 21 (GPIO 09).
+		/// </summary>
 		V2_Pin21 = 9,
 
+		/// <summary>
+		/// Rev2 Pin 22 (GPIO 25).
+		/// </summary>
 		V2_Pin22 = 25,
 
+		/// <summary>
+		/// Rev2 Pin 23 (GPIO 11).
+		/// </summary>
 		V2_Pin23 = 11,
 
+		/// <summary>
+		/// Rev2 Pin 24 (GPIO 08).
+		/// </summary>
 		V2_Pin24 = 8,
 
+		/// <summary>
+		/// Rev2 Pin 26 (GPIO 07).
+		/// </summary>
 		V2_Pin26 = 7,
 		#endregion
 
 		#region Board Revision 2 New Plug P5
+		/// <summary>
+		/// Rev2 P5 header GPIO 28 (P5-03).
+		/// </summary>
 		V2_P5_Pin03 = 28,
 
+		/// <summary>
+		/// Rev2 P5 header GPIO 29 (P5-04).
+		/// </summary>
 		V2_P5_Pin04 = 29,
 
+		/// <summary>
+		/// Rev2 P5 header GPIO 30 (P5-05).
+		/// </summary>
 		V2_P5_Pin05 = 30,
 
+		/// <summary>
+		/// Rev2 P5 header GPIO 31 (P5-06).
+		/// </summary>
 		V2_P5_Pin06 = 31
 		#endregion
 	}
